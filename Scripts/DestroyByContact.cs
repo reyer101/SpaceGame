ab5f9e130d6d4a66e7e8e2752bd0c758ff621b37
Alec <reyer101@mail.chapman.edu>
2016-11-28 13:38:38 -0800

    SpaceGame upload

---
+++ Scripts/DestroyByContact.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController Script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+            if (tag == "Star")
+            {
+                Destroy(other.gameObject);
+                return;
+            }
+        }
+        if (other.tag == "Star")
+        {
+            return;
+        }
+        if (other.tag == "Hazard")
+        {
+            Destroy(other.gameObject);
+            Instantiate(explosion, transform.position, transform.rotation);
+            return;
+        }
+        if (other.tag == "Bolt" && tag == "Star")
+        {
+            Destroy(other.gameObject);
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        gameController.AddScore(scoreValue);       
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+	
+}
---
+++ Scripts/StarMover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarBoundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class StarMover : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;    
+    public StarBoundary boundary;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.right * speed;                
+    }         
+    void FixedUpdate()
+    {
+        if (rb.position.x < 6 && rb.velocity == transform.right * speed)
+        {
+            rb.velocity = transform.right * speed;
+        }
+        if (rb.position.x >= 6)
+        {
+            rb.velocity = transform.right * -speed;
+            return;
+        }
+        if (rb.position.x <= -6 && rb.velocity == transform.right * -speed)
+        {
+            rb.velocity = transform.right * speed;
+        }
+    }
+}
